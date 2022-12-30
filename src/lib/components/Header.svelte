717b76ce679c9143d235d8e24cd83edd1577ac9c
Joni Puljujärvi <settheoreticalpineapple@users.noreply.github.com>
2022-12-30 14:26:01 +0200

    Updated Header to show background image
    Header component now picks a random banner to display as background image.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,1 +1,19 @@
-<h1>Klaanon Nuva</h1>
+<script>
+    let url =
+        "https://klaanon.fi/wp/wp-content/themes/klaanon/header-images/"
+        + Math.round(1 + 6*Math.random())
+        + ".png";
+</script>
+
+<header>
+    <h1>Klaanon Nuva</h1>
+</header>
+
+<style>
+    header {
+        background-image: url({url);
+    }
+    h1 {
+        font-size: 0;
+    }
+</style>
