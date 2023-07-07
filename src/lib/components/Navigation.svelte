8a3f1e817c6f37fdb0d3e44513b3d731b46564b5
Ville Talonpoika <ville@talonpoika.net>
2023-07-07 21:11:06 +0300

    Update muotoiluopas link

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -8,7 +8,7 @@
   <NavItem target="/">Etusivu</NavItem>
   <NavItem target="/categories">Kategoriat</NavItem>
   <NavItem target="/tags">Tagit</NavItem>
-  <NavItem target="/posts/muotoiluohje">Muotoilu</NavItem>
+  <NavItem target="/posts/muotoiluopas">Muotoiluopas</NavItem>
   <NavItem target="https://arkisto.klaanon.fi/soundtracks/">Soundtrackit</NavItem>
   <NavItem target="/profile">Profiili</NavItem>
   <NavItem target="/search">Haku</NavItem>
