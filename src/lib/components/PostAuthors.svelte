695020901861a1d0a665e23de3861b5f027540da
Ville Talonpoika <ville@talonpoika.net>
2023-07-20 16:05:50 +0300

    Post authors

---
+++ src/lib/components/PostAuthors.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { Post } from "$lib/types";
+
+  export let post: Post;
+</script>
+
+<div id="authors">
+  {#if post.authors.length > 0}
+    <div>
+      <span class="material-icons">history_edu</span> {post.authors.join(", ")}
+    </div>
+  {/if}
+  {#if post.artists.length > 0}
+    <div>
+      <span class="material-icons">palette</span> {post.artists.join(", ")}
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  #authors {
+    margin-top: 3rem;
+    text-align: center;
+    font-style: italic;
+  }
+
+  .material-icons {
+    font-size: 1rem;
+    position: relative;
+    top: 0.2rem;
+  }
+</style>
--- src/lib/components/PostFooter.svelte
+++ src/lib/components/PostFooter.svelte
@@ -2,11 +2,19 @@
   import type { Post } from "$lib/types";
   import PostTags from "$lib/components/PostTags.svelte";
   import PostComments from "$lib/components/PostComments.svelte";
+  import PostAuthors from "$lib/components/PostAuthors.svelte";
 
   export let post: Post;
 </script>
 
 <footer id="post-footer">
+  <PostAuthors {post} />
   <PostTags tags={post.tags} />
   <PostComments {post} />
 </footer>
+
+<style lang="scss">
+  footer {
+    margin-top: 6rem;
+  }
+</style>
