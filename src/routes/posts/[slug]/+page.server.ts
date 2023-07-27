2b44e4f249c465d6c473a2e17bbd66b4b99bcc66
Ville Talonpoika <ville@talonpoika.net>
2023-07-27 21:51:21 +0300

    Fix HTML validator bug

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -3,12 +3,13 @@ import { getPostBySlug } from "$lib/util/database";
 import type { Post } from "$lib/util/types";
 import { t } from "$lib/translations";
 import { validateHTML } from "$lib/util/html-validator";
+import { browser } from "$app/environment";
 
 export const load: Load = async ({ fetch, params, url }): Promise<Post | null> => {
 	if (params.slug) {
 		const isPreview = url.searchParams.get("preview") != null;
 		const post = await getPostBySlug(fetch, params.slug);
-		if (isPreview && post) {
+		if (isPreview && post && !browser) {
 			post.validationResult = await validateHTML(post.content);
 		}
 
