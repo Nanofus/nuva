9b780fe3f59b41eb54c2c8cf9f0c5cb3d0168efc
Ville Talonpoika <ville@talonpoika.net>
2023-07-27 12:14:07 +0300

    Fix error throw

--- src/routes/categories/[category]/+page.ts
+++ src/routes/categories/[category]/+page.ts
@@ -2,7 +2,6 @@ import { error, type Load } from "@sveltejs/kit";
 import { getPostListByCategory } from "$lib/util/database";
 import type { PostListByCategoryResponse } from "$lib/util/types";
 import { t } from "$lib/translations";
-import { CodeError } from "$lib/util/util";
 
 export const load: Load = async ({ fetch, params }): Promise<PostListByCategoryResponse> => {
 	if (params.category) {
@@ -12,5 +11,5 @@ export const load: Load = async ({ fetch, params }): Promise<PostListByCategoryR
 		}
 	}
 
-	throw error(new CodeError({ code: 404, message: t.errors.e404 }));
+	throw error(404, t.errors.e404);
 };
