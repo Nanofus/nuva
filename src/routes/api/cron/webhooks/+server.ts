7c675663c1dd24ca398535d288872412cb82df3e
Ville Talonpoika <ville@talonpoika.net>
2023-08-02 22:49:59 +0300

    Remove todo comment

--- src/routes/api/cron/webhooks/+server.ts
+++ src/routes/api/cron/webhooks/+server.ts
@@ -4,7 +4,6 @@ import { latestCommentHook, latestPostHook } from "$lib/server/webhooks";
 import { t } from "$lib/util/translations";
 import { globalConfig } from "$lib/util/config";
 
-// TODO: Require a secret key to be passed in the request
 export const GET: RequestHandler = async ({ url }) => {
   const password = url.searchParams.get("password");
   if (password !== globalConfig.webhooks.password) throw error(401, t.errors.e401);
