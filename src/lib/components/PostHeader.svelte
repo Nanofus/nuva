e97569821470e6329e2b20a9ae04a34028cc8bd5
Ville Talonpoika <ville@talonpoika.me>
2025-01-07 01:00:22 +0200

    Add fix

--- src/lib/components/PostHeader.svelte
+++ src/lib/components/PostHeader.svelte
@@ -25,7 +25,7 @@
       <span class="author-list">
         {#each post.coAuthors as author, i}
           <a href="/authors/{encodeURI(author)}">{author}</a
-          >{#if i !== post.coAuthors.length - 1}<span>,&nbsp;</span>{/if}
+          >{#if i !== post.coAuthors.length - 1},&#32;{/if}
         {/each}
       </span>
     </span>
