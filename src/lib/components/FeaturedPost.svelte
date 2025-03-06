db9a73847120090ad920c8d3d3ae7de7312fd615
Ville Talonpoika <ville@talonpoika.me>
2025-03-06 23:29:53 +0200

    Featured post line height fix

--- src/lib/components/FeaturedPost.svelte
+++ src/lib/components/FeaturedPost.svelte
@@ -42,7 +42,7 @@
       margin-bottom: 0;
       text-align: left;
       text-shadow: var(--banner-shadow);
-      line-height: 1.7rem;
+      line-height: var(--line-height, 1.7rem);
 
       a {
         font-size: var(--header-size, 0.75em);
